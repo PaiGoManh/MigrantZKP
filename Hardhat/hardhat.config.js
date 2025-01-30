b40bc990abd3d54453abd2f1a35ef0496021fea7
Paigomanh <rahulrahulsajeevan007@gmail.com>
2025-01-30 12:14:08 +0530

    added attestation

--- Hardhat/hardhat.config.js
+++ Hardhat/hardhat.config.js
@@ -1,12 +1,22 @@
-require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config();
+/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomicfoundation/hardhat-toolbox")
+require('dotenv').config();
 
 module.exports = {
+  defaultNetwork:"infura",
+  networks:{
+    localhost:{
+      url:"http://127.0.0.1:8545/"
+    },
+    infura:{
+      url:process.env.INFURA_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    }
+  },
   solidity: "0.8.20",
-  networks: {
-    arbitrum: {
-      url: process.env.ARBITRUM_RPC,
-      accounts: [process.env.PRIVATE_KEY]
+  etherscan: {
+    apiKey: {
+      sepolia: process.env.apiKey,
     }
   }
 };
